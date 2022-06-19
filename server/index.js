60a31f1393f315ae9631d10500dc15b61292df5b
carsalcedo <fercarrera07@gmail.com>
2022-06-19 17:42:31 -0400

    to deployd

--- server/index.js
+++ server/index.js
@@ -17,7 +17,9 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
-//const CONNECTION_URL = 'mongodb+srv://fercarrera:4s14nsucr3@cluster0.onank.mongodb.net/?retryWrites=true&w=majority';
+app.get('/', (req, res) =>{
+    res.send('My API is Running')
+})
 
 const PORT = process.env.PORT || 5000;
 
